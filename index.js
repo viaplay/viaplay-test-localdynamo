da231b449ccc65091bda2e10e43325e2f494beb8
Joakim J. Rapp <joakim.rapp@viaplay.com>
2015-02-03 13:50:08 +0100

    removed log

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 module.exports = function( pub, ioc, callback ) {
 	ioc.getContainer()
-		.registerIocLog( 'log' )
 		.registerResolved( {
 			settings: require( './config/settings.json' ),
 			spawn: require( 'child_process' ).spawn
 		} )
 		.autoRegisterPath( './lib' )
-		.resolveAllAndInject( function( dynamoDb, dynamoDbLocalManager ) {
+		.inject( function( dynamoDb, dynamoDbLocalManager ) {
 			pub.dynamoDb = dynamoDb;
 			pub.start = dynamoDbLocalManager.start;
 			callback();
