459de8436f5b9c930724a6b9d8900f0cdcc3acaa
Joakim J. Rapp <joakim.rapp@viaplay.com>
2015-07-07 09:09:23 +0200

    new implementation

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-module.exports = function( pub, ioc, callback ) {
-	ioc.getContainer()
-		.registerResolved( {
-			settings: require( './config/settings.json' ),
-			spawn: require( 'child_process' ).spawn
-		} )
-		.autoRegisterPath( './lib' )
-		.inject( function( dynamoDb, dynamoDbLocalManager ) {
-			pub.dynamoDb = dynamoDb;
-			pub.start = dynamoDbLocalManager.start;
-			callback();
+var aws = require( 'aws-sdk' );
+module.exports = function() {
+	var start = function( port, callback ) {
+		var child = spawn( 'java', [
+			'-Djava.library.path=' + path.resolve( __dirname, '../dynamodb_local/DynamoDBLocal_lib' ),
+			'-jar',
+			path.resolve( __dirname, '../dynamodb_local/DynamoDBLocal.jar' ),
+			'-inMemory',
+			'-port',
+			port.toString()
+		] );
+		process.on( 'exit', function() { child.kill(); } );
+		process.on( 'SIGINT', function() { process.exit(); } );
+		( function connect() {
+			net
+				.connect( { port: settings.port }, function() {
+					this.end();
+					callback();
+				} )
+				.on( 'error', function( err ) {
+					setTimeout( connect, 10 );
+				} );
+		} )();
+	};
+	return function( port, callback ) {
+		start( port, function() {
+			aws.config.update( {
+				sslEnabled: false,
+				endpoint: 'localhost:' + settings.port.toString(),
+				accessKeyId: 'xxx',
+				secretAccessKey: 'xxx',
+				region: 'xxx'
+			} );
+			callback( undefined, new aws.DynamoDB() );
 		} );
+	};
 };
--- tests/unit/sample.js
+++ tests/unit/sample.js
@@ -1,8 +1,1 @@
 var assert = require( 'assert' );
-describe( 'Sample', function() {
-	describe( 'Something', function() {
-		it( 'Should do nothing', function() {
-			assert.ok( true );
-		} );
-	} );
-} );
